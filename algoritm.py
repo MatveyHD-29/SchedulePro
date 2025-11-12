07ed549682876f1243447f1ea511088df1ec9d37
MatveyHD-29 <139860744+MatveyHD-29@users.noreply.github.com>
2025-11-12 15:48:38 +0300

    Add files via upload

--- algoritm.py
+++ algoritm.py
@@ -4,7 +4,7 @@ def generate_schedule(file_name, days):
     info_about_classes, info_about_teachers = get_data_from_billing(file_name, days)
     schedule_classes = {name: {day: {i: None for i in range(1, 9)} for day in days} for name in info_about_classes}
     schedule_teachers = {name: {day: {i: None for i in range(1, 9)} for day in days} for name in info_about_teachers}
-    return info_about_classes
+    return info_about_teachers
 
 days = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница']
-print(generate_schedule("5E22B510.xlsx", days)["10и"])
+print(generate_schedule("5E22B510.xlsx", days)["Курицына О.А."])
