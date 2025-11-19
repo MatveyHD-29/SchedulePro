9eb565f7e9d5ae81a55b0add3a554e80dd0f9a34
MatveyHD-29 <139860744+MatveyHD-29@users.noreply.github.com>
2025-11-19 16:11:08 +0300

    Add files via upload

--- new_read_data.py
+++ new_read_data.py
@@ -14,20 +14,51 @@ def load_sheet(file_name, sheet_name, method):
             vr = []
             for cell in row:
                 cell_value = cell.value
-                if cell_value != None:
-                    vr.append(str(cell_value))
+                vr.append(str(cell_value))
             mas.append(vr)
         else:
             break
     return mas
 
-def load_teachers_classes():
+def get_time_for_moving(file_name):
+    mas = load_sheet(file_name, "Корпуса", "rows")
+    time_for_moving = {}
+    for name in mas[0]:
+        if name != "-":
+            for build in mas:
+                if build[0] == name:
+                    vr = {}
+                    c = 0
+                    for hour in build:
+                        if hour != name and hour != "-":
+                            vr[mas[0][c]] = int(hour)
+                        c += 1
+                    time_for_moving[name] = vr
+    return time_for_moving
+
+def load_teachers_and_classes():
     mas = load_sheet("aaa.xlsx", "База данных", "cols")
-    teachers_list = mas[0][1:]
-    classes_list = mas[1][1:]
-    print(classes_list)
+    teachers_list = []
+    for row in mas[0][1:]:
+        if row != "None":
+            teachers_list.append(row)
+    classes_list = []
+    for row in mas[1][1:]:
+        if row != "None":
+            classes_list.append(row)
+    return teachers_list, classes_list
 
 def load_info_about_teachers():
-    pass
+    teacher_list, classes_list = load_teachers_and_classes()
+    mas = load_sheet("aaa.xlsx", "Тарификация", "rows")
+    info_about_teachers = {}
+    for row in mas[1:]:
+        c = 0
+        for hour in row[2:-4]:
+            if hour != "None":
+                class_name = classes_list[c - 2]
+                print(hour, class_name)
+            c += 1
 
-print(load_teachers_classes())
+
+load_info_about_teachers()
