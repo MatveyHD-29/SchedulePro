d073aaf462850f3cf8ab071e8ff580f16494c69b
MatveyHD-29 <139860744+MatveyHD-29@users.noreply.github.com>
2025-12-03 15:44:35 +0300

    Add files via upload

--- algoritm.py
+++ algoritm.py
@@ -1,10 +1,16 @@
-from read_data import get_data_from_billing
+from read_data import load_info
 
 def generate_schedule(file_name, days):
-    info_about_classes, info_about_teachers = get_data_from_billing(file_name, days)
+    info_about_teachers, info_about_classes = load_info(file_name)
     schedule_classes = {name: {day: {i: None for i in range(1, 9)} for day in days} for name in info_about_classes}
     schedule_teachers = {name: {day: {i: None for i in range(1, 9)} for day in days} for name in info_about_teachers}
+    print(schedule_classes)
+    for class_name in info_about_classes.keys():
+        for day in schedule_classes[class_name]:
+            for number in schedule_classes[class_name][day]:
+                if day != "Понедельник" and number != 1:
+                    pass
     return info_about_teachers
 
 days = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница']
-print(generate_schedule("5E22B510.xlsx", days)["Курицына О.А."])
+generate_schedule("aaa.xlsx", days)
